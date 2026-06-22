3b081ff6080836f594737cb096aa1e1d2a8c50fb
mhjang <mhjang@plateer.com>
2026-06-22 18:09:17 +0900

    feat: update ComPass configuration and index page3

--- static/config.js
+++ static/config.js
@@ -1,5 +1,5 @@
 window.COMPASS_CONFIG = Object.freeze({
   appName: "ComPass",
-  appSubtitle: "Computer Science + Compass · 학생들의 길잡이",
+  appSubtitle: "Computer Science × Compass\n· 학생들의 길잡이",
   timeZone: "Asia/Seoul",
 });
